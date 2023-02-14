18b28fe9bf5de6e7df8640126ff78c7bc172ffd3
H-goto16 <h.goto.engineer@gmail.com>
2023-02-14 17:27:37 +0900

    feat: add speech_and_NLP as submodule

--- src/audio.py
+++ src/audio.py
@@ -3,21 +3,18 @@
 
 import rospy
 from std_msgs.msg import String
+from ..speech_and_NLP.src.tools.text_to_speech.textToSpeech import textToSpeech
 
 class Audio():
     def __init__(self):
         self.sub = rospy.Subscriber("/audio", String, self.callback)
     
     def callback(self, msg):
-        """
-        print(msg.data)
-        
-        text to speach here
-        """
-        pass
+        textToSpeech(msg.data)
 
 if __name__ == '__main__':
     rospy.init_node("audio")
     audio = Audio()
+    
     while not rospy.is_shutdown():
         rospy.Rate(10).sleep()
