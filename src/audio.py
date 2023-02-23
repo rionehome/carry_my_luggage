8b61ebbbdd5c1f063c4928e743fd63c858b24be9
H-goto16 <h.goto.engineer@gmail.com>
2023-02-23 19:49:16 +0900

    fix: no such file or directory

--- src/audio.py
+++ src/audio.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import rospy
-from std_msgs.msg import String
+# import rospy
+# from std_msgs.msg import String
 
 import sys
 import os
@@ -11,16 +11,18 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from speech_and_NLP.src.tools.text_to_speech.textToSpeech import textToSpeech
 
-class Audio():
-    def __init__(self):
-        self.sub = rospy.Subscriber("/audio", String, self.callback)
-    
-    def callback(self, msg):
-        textToSpeech(msg.data)
+textToSpeech("test")
 
-if __name__ == '__main__':
-    rospy.init_node("audio")
-    audio = Audio()
+# class Audio():
+#     def __init__(self):
+#         self.sub = rospy.Subscriber("/audio", String, self.callback)
     
-    while not rospy.is_shutdown():
-        rospy.Rate(10).sleep()
+#     def callback(self, msg):
+#         textToSpeech(msg.data)
+
+# if __name__ == '__main__':
+#     rospy.init_node("audio")
+#     audio = Audio()
+    
+#     while not rospy.is_shutdown():
+#         rospy.Rate(10).sleep()
