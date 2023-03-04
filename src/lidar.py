426f8c5b8a23b9392a1040456f0db5f194816966
Hara-kun <is0575ve@ed.ritsumei.ac.jp>
2023-03-04 15:38:13 +0900

    initial commit

--- src/lidar.py
+++ src/lidar.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import Float32
 from sensor_msgs.msg import LaserScan
 from carry_my_luggage.msg import LidarData
 import math
@@ -11,18 +10,15 @@ PIZZA = 24
 class Lidar():
     def __init__(self):
         self.pub = rospy.Publisher('/lidar', LidarData, queue_size=1)
-    
+        self.main()
+        
     def remove_inf(self, ranges):
         f_ranges = []
-
         for i in ranges:
             if not math.isinf(i):
                 f_ranges.append(i)
 
         return f_ranges
-    
-    def average_ranges(self, ranges):
-        return sum(ranges) / len(ranges)
     
     def get_distance(self):
         scan = rospy.wait_for_message('/scan', LaserScan)
@@ -38,17 +34,45 @@ class Lidar():
         for i in range(pizza):
             start = dots * i
             end = start + dots
-            distance.append(self.average_ranges(ranges[start:end]))
+            r = ranges[start:end]
+            a = sum(r) / len(r)
+            distance.append(a)
 
         # rospy.loginfo(distance)
 
         return distance
+    
+    def get_direction(self, ranges):
+        mn = min(ranges)
+        mn_index = ranges.index(mn)
+        
+        if mn_index <= 5 or mn_index >= 18:
+            front_back = "front"
+        else:
+            front_back = "back"
+        
+        if mn_index <= 11:
+            left_right = "left"
+        else:
+            left_right = "right"
+            
+        direction = [front_back, left_right]
+        
+        return direction
+        
+    def main(self):
+        while not rospy.is_shutdown():
+            l = LidarData()
+            l.distance = self.get_distance()
+            direction = self.get_direction(l.distance)
+            l.front_back = direction[0]
+            l.left_right = direction[1]
+            print(direction)
+            self.pub.publish(l)
+            rospy.Rate(10).sleep()
+
 
 if __name__ == '__main__':
     rospy.init_node("lidar")
     lidar = Lidar()
-    while not rospy.is_shutdown():
-        l = LidarData()
-        l.distance = lidar.get_distance()
-        lidar.pub.publish(l)
-        rospy.Rate(10).sleep()
+    
