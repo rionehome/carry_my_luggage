441523c26337308e89fbf40592fb160f5b47f631
Hara-kun <78362575+Hara-kun@users.noreply.github.com>
2023-03-04 14:38:42 +0900

    initial commit

--- src/move_arm.py
+++ src/move_arm.py
@@ -5,9 +5,8 @@ import rospy
 from carry_my_luggage.msg import ArmAction
 from carry_my_luggage.srv import MoveArm
 import numpy as np
-from math import pi, radians, sqrt
+import math
 import time
-import traceback
 
 class Take_a_Bag():
     def __init__(self):
@@ -17,57 +16,94 @@ class Take_a_Bag():
         rospy.spin()
     
     def main(self, message):
-        #Linkの長さ 
+        # Linkの長さ(固定値)
         L1 = 13
         L2 = 15
         L3 = 15
-        #アーム手先座標
+        # アーム手先座標
         Px = message.x
         Py = message.y
-        # L3の角度指定。(x軸とのなす角)
-        tht = radians(message.deg)
+        # L3の角度指定(x軸とのなす角), deg -> rad
+        tht = math.radians(message.deg)
         
+        pi = math.pi
+        # 付け根の角度
+        th0 = pi - 0.01
+        
+        # init
+        if message.x == 0 and message.y == 0:
+            th1 = -pi/3
+            th2 = pi/4
+            th3 = pi/2 - 0.01
+            grip = 4
+        
+            # 以下の設定でアームを動かす
+            armAction = ArmAction()
+            armAction.joint = [th0, -pi/3, th2, th3]
+            armAction.gripper = grip
+            self.arm_pub.publish(armAction)
+            time.sleep(3)   # アームのデフォルト動作時間が3秒
+            
+            th2 = pi/6
+            th3 = pi/4
+            armAction.joint = [th0, th1, th2, th3]
+            armAction.gripper = grip
+            self.arm_pub.publish(armAction)
+            time.sleep(3)   # アームのデフォルト動作時間が3秒
+            
+            return 0
+        
+        # gripの設定
         grip = message.grip
         if grip < 0 or grip > 4:
-            rospy.loginfo("gripper: Not 0-4")
+            rospy.loginfo("move_arm: grip is Not 0-4")
             return -1
         
-        Px = Px - L3*np.cos(tht)
-        Py = Py - L3*np.sin(tht)
+        # 3link -> 2link
+        tPx = Px - L3*np.cos(tht)
+        tPy = Py - L3*np.sin(tht)
+        
+        # 逆運動学で計算
         try:
-            th1 = np.arccos((Px*Px+Py*Py+L1*L1-L2*L2)/(2*L1*sqrt(Px*Px+Py*Py)))+np.arctan(Py/Px)
-            th2 = np.arctan((Py-L1*np.sin(th1))/(Px-L1*np.cos(th1)))-th1
+            c = math.sqrt((tPx*tPx) + (tPy*tPy))
+            # への字の解
+            th2 = - math.acos((c*c - L1*L1 - L2*L2) / (2*L1*L2))
+            th1 = math.atan2(tPy, tPx) + math.acos((c*c + L1*L1 - L2*L2) / (2*L1*c))
+            if abs(math.pi/2-th1) > math.pi/2:
+                # 逆への字の解
+                th2 = math.acos((c*c - L1*L1 - L2*L2) / (2*L1*L2))
+                th1 = math.atan2(tPy, tPx) - math.acos((c*c + L1*L1 - L2*L2) / (2*L1*c))
             th3 = tht - th1 -th2
+
         except:
-            rospy.loginfo(traceback.print_exc())
+            # 主に目標座標がアームの可動域外になった場合
+            rospy.loginfo("move_arm: No solution")
             return -1
+        
+        # 念のため、順運動学で検算
+        x = L1*math.cos(th1) + L2*math.cos(th1+th2) + L3*math.cos(th1+th2+th3)
+        y = L1*math.sin(th1) + L2*math.sin(th1+th2) + L3*math.sin(th1+th2+th3)
+        if abs(Px - x) > 1.0 or abs(Py - y) > 1.0:
+            rospy.loginfo("move_arm: No solution")
+            return -1
+        
         
         # 逆運動学の角度からロボットの角度に変換
         th1 = -th1 + pi/2
         th2 = -th2 - pi/2
         th3 = -th3
         
-        # 角度オーバー
-        if th1 > pi/2 or th1 < -pi/2:
-            th1 = None
-        if th2 > pi/2 or th2 < -pi/2:
-            th2 = None
-        if th1 == None or th2 == None or th3 == None:
-            rospy.loginfo(traceback.print_exc())
+        # ロボットが安全に動作する角度をオーバー
+        if abs(th1) > math.pi/2 or abs(th2) > math.pi/2 or abs(th3) > math.pi/2:
+            rospy.loginfo("move_arm: No realistic solution") 
             return -1
         
-        #init
-        if message.x == 0 and message.y == 0:
-            th1 = -pi/4
-            th2 = pi/4
-            th3 = pi/4
-            grip = 4
-        
+        # 以下の設定でアームを動かす
         armAction = ArmAction()
-        armAction.joint = [0, th1, th2, th3]
+        armAction.joint = [th0, th1, th2, th3]
         armAction.gripper = grip
         self.arm_pub.publish(armAction)
-        time.sleep(3)
+        time.sleep(3)   # アームのデフォルト動作時間が3秒
         
         return 0
     
