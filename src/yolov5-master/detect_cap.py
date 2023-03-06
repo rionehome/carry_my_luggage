36982d74649bdcb7293823a884d7ffee40ad8622
dammy <dammy@example.com>
2023-03-06 18:13:00 +0900

    fix: model path

--- src/yolov5-master/detect_cap.py
+++ src/yolov5-master/detect_cap.py
@@ -374,7 +374,7 @@ def run(
 def parse_opt():
     parser = argparse.ArgumentParser()
     #parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5s.pt', help='model path or triton URL')
-    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'models/pprbg_dtcr.pt', help='model path or triton URL')
+    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / '../yolov5s.pt', help='model path or triton URL')
     #parser.add_argument('--source', type=str, default=ROOT / 'data/images', help='file/dir/URL/glob/screen/0(webcam)')
     parser.add_argument('--source', type=str, default=0, help='file/dir/URL/glob/screen/0(webcam)')
     parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml path')
