6aa03ad858d05b8eff87f5e196aeee71b099a7a5
alienware <alienware@example.com>
2023-04-22 18:28:23 +0900

    add: add control system

---
+++ src/control_system.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+
+import rospy
+from geometry_msgs.msg import Twist
+
+class ControlSystem():
+    def __init__(self):
+        rospy.init_node("control_system")
+        self.velocity_pub = rospy.Publisher("/mobile_base/commands/velocity", Twist, queue_size=1)
+        self.cmd_vel_sub = rospy.Subscriber("/control_system/cmd_vel", Twist, self.cmd_vel_callback)
+    
+    def cmd_vel_callback(self, msg):
+        x = msg.linear.x
+        z = msg.angular.z
+
+        self.velocity_pub.publish(msg)
+
+if __name__ == "__main__":
+    try:
+        contorlSystem = ControlSystem()
+        rospy.spin()
+    
+    except KeyboardInterrupt:
+        pass
