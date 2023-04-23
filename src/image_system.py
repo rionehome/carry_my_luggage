8e0ba245fdf242da8d635213bf2af82fb239e2ac
alienware <alienware@example.com>
2023-04-23 16:20:42 +0900

    feat: detect hand direction

--- src/image_system.py
+++ src/image_system.py
@@ -3,10 +3,20 @@
 
 import rospy
 
+from carry_my_luggage.srv import HandDirection
+from finger_direction import get_direction
+
 
 class ImageSystem:
     def __init__(self):
         rospy.init_node("image_system")
+        self.hand_direction_srv = rospy.Service(
+            "/image_system/hand_direction", HandDirection, self.hand_direction_callback
+        )
+
+    def hand_direction_callback(self, msg):
+        rospy.loginfo("image_system: detecting hand direction")
+        return get_direction(msg.n)
 
 
 if __name__ == "__main__":
