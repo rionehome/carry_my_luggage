8287397ba098df68950070c8abd16ec37ee0d8d8
alienware <alienware@example.com>
2023-04-27 20:52:42 +0900

    remove: remove comments

--- src/image_system.py
+++ src/image_system.py
@@ -54,10 +54,6 @@ class ImageSystem:
             self.person_detect()
 
     def person_detect(self):
-        # img = cv2.imread('hiroyuki.jpg')
-
-        # print(model.names)
-
         ret, img = self.cap.read()  # 画像の大きさを取得するために1度だけ最初によびだす。
 
         # 人が写っていない前提で初期化する
@@ -112,8 +108,6 @@ class ImageSystem:
                         xmax = obj.xmax[i]
                         ymax = obj.ymax[i]
 
-                        # print("name =", name, "xmin =", xmin, "ymin =", ymin, "xmax =", ymax, "ymin =", ymax)
-
                         w = xmax - xmin  # 矩形の幅
                         h = ymax - ymin  # 矩形の高さ
                         c_x = (xmax + xmin) / 2  # 矩形の中心のx座標
@@ -138,46 +132,6 @@ class ImageSystem:
                         elif h <= 350:
                             print("far")
                             self.person_detect_distance_pub.publish("far")
-
-                        # if w < 350:
-                        #     # print(str(i) + "番目の人が遠い")
-                        #     robo_p_dis = 0  # ロボットは人が中央に来るまで前に進む
-
-                        # elif w >= 350 and w <= 550:
-                        #     # print(str(i) + "番目の人が中央の距離")
-                        #     robo_p_dis = 1  # ロボットはそのまま
-
-                        # elif w > 550:
-                        #     # print(str(i) + "番目の人が近い")
-                        #     robo_p_dis = 2  # ロボットは人が中央に来るまで後ろに下がる
-
-                        # if c_x < width / 3:
-                        #     # print(str(i) + "番目の人が左にいる")
-                        #     robo_p_drct = 0  # ロボットは人が中央に来るまで左回りする
-
-                        # elif c_x > width / 3 and c_x < width * 2 / 3:
-                        #     # print(str(i) + "番目の人が中央の方向")
-                        #     robo_p_drct = 1  # ロボットはそのまま
-
-                        # elif c_x > width * 2 / 3:
-                        #     # print(str(i) + "番目の人が右にいる")
-                        #     robo_p_drct
-
-                        print("=====")
-                        print("距離=" + str(robo_p_dis))
-                        print("方向=" + str(robo_p_drct))
-                        print("w", w)
-                        print("h", h)
-                        print("c_x", c_x)
-                        print("c_y", c_y)
-                        print("=====")
-
-                    # ロボットは人が中央に来るまで右回りする
-
-                    # print("距離:" + str(robo_p_dis) + "、方向:" + str(robo_p_drct))
-                    # print("(x_min=" + str(xmin) + ", y_min=" + str(ymin) + ")" + "(x_max=" + str(xmax) + ", y_max=" + str(ymax) + ")")
-
-                    # print("名前" + str(name) + "幅:" + str(w) + ", 高さ:" + str(h))
 
             person_count = 0  # 判定するための変数を初期化する
 
