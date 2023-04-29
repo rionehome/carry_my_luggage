9ba1e4c19a43a2763d64b3ee9239030c63faf9cb
alienware <alienware@example.com>
2023-04-29 16:31:57 +0900

    refactor: refactor person_detect()

--- src/image_system.py
+++ src/image_system.py
@@ -80,90 +80,55 @@ class ImageSystem:
     def person_detect(self):
         ret, img = self.cap.read()  # 画像の大きさを取得するために1度だけ最初によびだす。
 
-        # 人が写っていない前提で初期化する
-        robo_p_dis = 3  # ロボットと人との距離感覚
-        robo_p_drct = 3  # ロボットと人との方向感覚
-
-        c = 0
-        heigh = 0  # カメラから取得した画像の高さを保持
-        width = 0  # カメラから取得した画像の幅を保持
-
         person_count = 0  # 人が写っているかどうかを判定するための変数
 
-        if self.is_person_detect_on:
-            ret, img = self.cap.read()
-            result = self.person_detect_model(img)
+        direction = ""
+        distance = ""
 
-            if c == 0:
-                height, width, _ = img.shape[:3]
-                print("高さ=" + str(height))
-                print("幅=" + str(width))
-                c += 1
+        ret, img = self.cap.read()
+        result = self.person_detect_model(img)
 
-            # 推論結果を取得
-            obj = result.pandas().xyxy[0]
+        # 推論結果を取得
+        obj = result.pandas().xyxy[0]
 
-            # 人が写っているかを調べる
-            for i in range(len(obj)):
-                if obj.name[i] == "person":
-                    person_count += 1
-                    break
+        # 人が写っているかを調べる
+        for i in range(len(obj)):
+            if obj.name[i] == "person":
+                person_count += 1
 
-            # 人が一人も写っていないとき
-            if person_count == 0:
+                # 人のときだけ計算することで無駄な計算を削減する。
+                xmin = obj.xmin[i]
+                ymin = obj.ymin[i]
+                xmax = obj.xmax[i]
+                ymax = obj.ymax[i]
 
-                # 探す指示を距離3方向3として与える
-                robo_p_dis = 3
-                robo_p_drct = 3
+                w = xmax - xmin  # 矩形の幅
+                h = ymax - ymin  # 矩形の高さ
+                c_x = (xmax + xmin) / 2  # 矩形の中心のx座標
+                c_y = (ymax + ymin) / 2  # 矩形の中心のy座標
 
-            # 人が写っているとき
-            if person_count == 1:
+                if c_x >= 0 and c_x <= WIDTH * (1 / 3):
+                    direction = "left"
+                elif c_x < WIDTH and c_x <= WIDTH * (2 / 3):
+                    direction = "center"
+                else:
+                    direction = "right"
 
-                # バウンディングボックスの情報を取得
-                for i in range(len(obj)):
+                if h >= 450:
+                    distance = "close"
+                elif h > 350 and h < 450:
+                    distance = "middle"
+                elif h <= 350:
+                    distance = "far"
 
-                    # 0番目の人(オペレータを想定している)について
-                    # ロボットから見たときの距離と方向について
-                    if obj.name[i] == "person" and i == 0:
+                self.person_detect_direction_pub.publish(direction)
+                self.person_detect_distance_pub.publish(distance)
 
-                        # 人のときだけ計算することで無駄な計算を削減する。
-                        xmin = obj.xmin[i]
-                        ymin = obj.ymin[i]
-                        xmax = obj.xmax[i]
-                        ymax = obj.ymax[i]
-
-                        w = xmax - xmin  # 矩形の幅
-                        h = ymax - ymin  # 矩形の高さ
-                        c_x = (xmax + xmin) / 2  # 矩形の中心のx座標
-                        c_y = (ymax + ymin) / 2  # 矩形の中心のy座標
-
-                        if c_x >= 0 and c_x <= WIDTH * (1 / 3):
-                            print("left")
-                            self.person_detect_direction_pub.publish("left")
-                        elif c_x < WIDTH and c_x <= WIDTH * (2 / 3):
-                            print("center")
-                            self.person_detect_direction_pub.publish("center")
-                        else:
-                            print("right")
-                            self.person_detect_direction_pub.publish("right")
-
-                        if h >= 450:
-                            print("close")
-                            self.person_detect_distance_pub.publish("close")
-                        elif h > 350 and h < 450:
-                            print("middle")
-                            self.person_detect_distance_pub.publish("middle")
-                        elif h <= 350:
-                            print("far")
-                            self.person_detect_distance_pub.publish("far")
-
-            person_count = 0  # 判定するための変数を初期化する
-
-            # バウンディングボックスを描画
-            result.render()
-            cv2.imshow("result", result.ims[0])
-            if cv2.waitKey(1) & 0xFF == ord("q"):
-                return
+        # バウンディングボックスを描画
+        result.render()
+        cv2.imshow("result", result.ims[0])
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            return
 
     def paperbag_detect_switch_callback(self, msg):
         if msg.data == "on" and self.is_paperbag_detect_on == False:
