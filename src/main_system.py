cd9027f2e832d4ea31991c343dab85a1c9badc1c
alienware <alienware@example.com>
2023-05-01 20:26:18 +0900

    add: add paperbag detection

--- src/main_system.py
+++ src/main_system.py
@@ -85,32 +85,89 @@ class MainSystem:
 
         self.audio_tts_client("どちらの紙袋を取りたいか指で指してください")
 
-        i = self.image_hand_detect_client(90)
-        i = i.direction
+        i = self.image_hand_detect_client(60)
+        direction = i.direction
 
         # 返ってくるiの値は指を指した人から見た方向
         # ロボットから見た方向を発話する
-        if i == "right":
+        if direction == "left":
             self.audio_tts_client("左ですね")
             self.audio_tts_client("左の紙袋を掴みます")
-        elif i == "left":
+        elif direction == "right":
             self.audio_tts_client("右ですね")
             self.audio_tts_client("右の紙袋を掴みます")
 
+        did_turn = False
+
         while True:
+            t = Twist()
+
             self.image_paperbag_detect_switch_pub.publish("on")
+
+            p_count = self.paperbag_count
+            p_width = self.paperbag_width
+            p_direction = self.paperbag_direction
+            h_count = self.holding_count
+            h_direction = self.holding_direction
+            h_width = self.holding_width
+
+            if h_count >= 2 and direction in h_direction and not did_turn:
+                if direction == "right":
+                    print("turning right")
+                    t.angular.z = -0.5
+                elif direction == "left":
+                    print("turning left")
+                    t.angular.z = 0.5
+                else:
+                    t.angular.z = 0
+                    did_turn = True
+
+                self.control_vel_pub.publish(t)
+            elif p_count >= 2:
+                max_width = 0
+                for i in range(p_count):
+                    if max_width < p_width[i]:
+                        max_width = p_width[i]
+                        direction = p_direction[i]
+
+                if max_width > 450:
+                    t.linear.x = 0
+                else:
+                    t.linear.x = 0.09
+
+                self.control_vel_pub.publish(t)
+
+            elif p_count == 1:
+                max_width = p_width[0]
+                d = p_direction[0]
+
+                if max_width > 450:
+                    t.linear.x = 0
+                else:
+                    t.linear.x = 0.09
+
+                if d == "right":
+                    t.angular.z = 0.7
+                elif d == "left":
+                    t.angular.z = -0.7
+                else:
+                    t.angular.z = 0
+
+                self.control_vel_pub.publish(t)
+
+                rospy.loginfo(max_width)
 
         while True:
             t = Twist()
             self.image_person_detect_switch_pub.publish("on")
 
-            count = self.person_detect_count
+            count = self.person_count
 
             if count == 1:
 
-                direction = self.person_detect_direction[0]
-                height = self.person_detect_height[0]
-                xmid = self.person_detect_xmid[0]
+                direction = self.person_direction[0]
+                height = self.person_height[0]
+                xmid = self.person_xmid[0]
 
                 # if direction == "right":
                 #     rospy.loginfo("right")
@@ -121,25 +178,26 @@ class MainSystem:
                 #     rospy.loginfo("left")
                 #     t.angular.z = 0.7
 
-                if xmid > (WIDTH / 2) - 20 and xmid < (WIDTH / 2) + 20:
+                if xmid > (WIDTH / 2) - 15 and xmid < (WIDTH / 2) + 15:
                     pass
                 elif xmid < WIDTH / 2:
-                    t.angular.z = 0.7
+                    t.angular.z = 0.5
                 elif xmid > WIDTH / 2:
-                    t.angular.z = -0.7
+                    t.angular.z = -0.5
 
                 if height >= 460:
                     t.linear.x = 0
                 elif height < 460 and height >= 410:
-                    t.linear.x = 0.1
+                    t.linear.x = 0.06
                 elif height < 410:
-                    t.linear.x = 0.15
+                    t.linear.x = 0.12
+
             elif count > 1:
-                max_height = max(self.person_detect_height)
-                index = self.person_detect_height.index(max_height)
+                max_height = max(self.person_height)
+                index = self.person_height.index(max_height)
 
-                height = self.person_detect_height[index]
-                xmid = self.person_detect_xmid[index]
+                height = self.person_height[index]
+                xmid = self.person_xmid[index]
 
                 if xmid > (WIDTH / 2) - 20 and xmid < (WIDTH / 2) + 20:
                     pass
@@ -148,11 +206,11 @@ class MainSystem:
                 elif xmid > WIDTH / 2:
                     t.angular.z = -0.7
 
-                if height >= 460:
+                if height >= 400:
                     t.linear.x = 0
-                elif height < 460 and height >= 410:
+                elif height < 400 and height >= 350:
                     t.linear.x = 0.1
-                elif height < 410:
+                elif height < 350:
                     t.linear.x = 0.15
             else:
                 t.linear.x = 0
